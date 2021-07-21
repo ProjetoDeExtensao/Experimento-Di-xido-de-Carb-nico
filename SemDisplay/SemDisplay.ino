00ea06c75be595b91e06199a5549d9cf46f107fb
ProjetoDeExtensao <87783215+ProjetoDeExtensao@users.noreply.github.com>
2021-07-21 17:36:26 -0300

    Add files via upload

---
+++ SemDisplay/SemDisplay.ino
@@ -0,0 +1,16 @@
+#define Rl 1000 //resistência interna do sensor (esse valor vem de fábrica)
+
+void setup(){ 
+  Serial.begin(9600); //inicia a comunicação serial com o computador
+
+  pinMode(A0, INPUT); //pino A2 é definido comoe ntrada analógica, no caso o valor do sensor
+} 
+  
+void loop(){   
+  float Vo = analogRead(A0) * 5; //cálculo da tensão sobre o resistor 
+  float Vo = Vo / 1024; 
+
+  float Rs = (5 - Vo) * Rl; //valor da resistência Rs
+  
+  Serial.println("Resistencia: " + String(Rs)); //mostra "Resistencia: " e o valor medido na frente 
+} 
